0b44708330b68625651a6209a7fad8bb66c4abb2
wizardwid <s2335@e-mirim.hs.kr>
2024-03-21 11:48:16 +0900

    02-01 C++ 기본예제

---
+++ 2024-Cpp-202/grammer/02-01 기본예제.cpp
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+int main() {
+	// cout : 출력개체, endl : 개행객체
+	std::cout << "Hello World"<< std::endl;
+	return 0;
+}
--- 2024-Cpp-202/grammer/연습예제.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include <stdio.h>
-int main() {
-	// 주석 테스트
-	printf("Hello World!");
-	return 0;
-}
