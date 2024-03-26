c7d9eca008bd9a191cdc1654762ab66feaaf3139
wizardwid <s2335@e-mirim.hs.kr>
2024-03-26 15:01:23 +0900

    배열의 이름은 시작주소 값을 나타낸다

---
+++ 2024-Cpp-202/grammer/포인터기초.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+//이름 공간 std::를 안해도 된다
+using namespace std;
+
+int main() {
+	int num = 3;
+	cout << num << endl;	// 3 (변수에 들어있는 값)
+	cout << &num << endl;	// 변수가 메모리 공간에 할당되어 있는 주소
+
+	int arr[5] = { 10, 20, 30, 40, 50 };
+	cout << arr << endl;	// 배열이 메모리 공간에 할당되어 있는 주소
+}
