12751932a4f6989a9965fc2b77503ea4d39213c7
wizardwid <s2335@e-mirim.hs.kr>
2024-03-26 16:02:44 +0900

    배열의 주소값

---
+++ 2024-Cpp-202/grammer/배열과 포인터.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+
+// 이름 공간 std::를 안해도 된다
+using namespace std;
+
+int main() {
+	int arr[4] = {10, 20, 30, 40};
+	char str[4] = "abc";
+
+	//주소값 4바이트 만큼의 차이
+	printf("%d %d\n", &arr[0], &arr[1]);
+	//주소값 1바이트 만큼의 차이
+	printf("%d %d", &str[0], &str[1]);
+
+	return 0;
+
+}
