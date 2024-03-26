a2d4cc54eb5c82f4318bfa1b01956a767de05a14
wizardwid <s2335@e-mirim.hs.kr>
2024-03-26 15:32:44 +0900

    call by value

---
+++ 2024-Cpp-202/grammer/call-by.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+// 이름 공간 std::를 안해도 된다
+using namespace std;
+
+void call_by_value(int val) {
+	// 새로운 지역변수 val을 만들고 거기에 값을 넣는다.
+	val = 10;
+}
+
+int main() {
+	int num = 4;
+	// num의 값은 바뀌지 않는다.
+	call_by_value(num);		
+	cout << num;
+	return 0;
+}
