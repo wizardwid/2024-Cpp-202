2a7e840d8d3f50b0683203dc48481ea07f74119f
wizardwid <s2335@e-mirim.hs.kr>
2024-03-28 13:11:13 +0900

    함수를 통해 배열의 값을 바꾸자 by pointer

---
+++ 2024-Cpp-202/grammer/배열과 포인터 그리고 함수.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+//배열의 모든 요소를 0으로 바꿔주는 함수
+void erase(int* ptr) {
+	for (int i = 0; i < length;; i++) {
+		ptr[i] = 0;
+	}
+}
+
+int main() {
+
+	int arr[4] = {10, 20, 30, 40};
+	int length = sizeof(arr) / sizeof(arr[0]);
+	erase(arr);
+
+	//배열의 모든 요소를 출력
+	for (int i = 0; i < 4; i++)
+		printf("%d", arr[i]);
+
+	return 0;
+
+}
