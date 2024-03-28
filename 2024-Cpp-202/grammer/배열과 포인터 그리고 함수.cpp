1a7359caafcc9efa5235f20f4bf8ab9a22ea9634
wizardwid <s2335@e-mirim.hs.kr>
2024-03-28 17:28:42 +0900

    파일 이동

--- 2024-Cpp-202/grammer/배열과 포인터 그리고 함수.cpp
+++ 2024-Cpp-202/grammer/배열과 포인터 그리고 함수.cpp
@@ -1,8 +1,8 @@
 ﻿#include <stdio.h>
 
 //배열의 모든 요소를 0으로 바꿔주는 함수
-void erase(int* ptr) {
-	for (int i = 0; i < length;; i++) {
+void erase(int* ptr, int length) {
+	for (int i = 0; i < length; i++) {
 		ptr[i] = 0;
 	}
 }
@@ -11,7 +11,7 @@ int main() {
 
 	int arr[4] = {10, 20, 30, 40};
 	int length = sizeof(arr) / sizeof(arr[0]);
-	erase(arr);
+	erase(arr, length);
 
 	//배열의 모든 요소를 출력
 	for (int i = 0; i < 4; i++)
