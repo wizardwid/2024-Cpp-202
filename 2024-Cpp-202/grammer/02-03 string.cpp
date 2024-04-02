062890b2f352372924bfe4acb97cc51e924e903f
wizardwid <s2335@e-mirim.hs.kr>
2024-04-02 16:05:16 +0900

    함수 오버로딩 진행 후

---
+++ 2024-Cpp-202/grammer/02-03 string.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+//이름 공간 std::를 안해도 된다
+using namespace std;
+
+int mul(int a, int b) {
+	return a * b;
+}
+
+float mul(float a, float b) {
+	return a * b;
+}
+
+int main() {
+	cout << mul(2, 2) << endl;
+	cout << mul(2.2f, 2.2f) << endl;
+	return 0;
+}
