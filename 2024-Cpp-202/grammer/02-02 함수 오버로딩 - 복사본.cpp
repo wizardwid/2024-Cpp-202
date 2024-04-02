a8474901b26d3f98f019a237c0b143b260957e15
wizardwid <s2335@e-mirim.hs.kr>
2024-04-02 16:04:19 +0900

    함수 오버로딩 진행 전

---
+++ 2024-Cpp-202/grammer/02-02 함수 오버로딩 - 복사본.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+//이름 공간 std::를 안해도 된다
+using namespace std;
+
+int int_mul(int a, int b) {
+	return a * b;
+}
+
+float float_mul(float a, float b) {
+	return a * b;
+}
+
+int main() {
+	cout << int_mul(2, 2) << endl;
+	cout << float_mul(2.2f, 2.2f) << endl;
+	return 0;
+}
