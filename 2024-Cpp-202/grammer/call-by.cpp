dd26ca0831008a12f02b63e488e6a109db49fb20
wizardwid <s2335@e-mirim.hs.kr>
2024-04-02 15:46:17 +0900

    call by reference

--- 2024-Cpp-202/grammer/call-by.cpp
+++ 2024-Cpp-202/grammer/call-by.cpp
@@ -3,15 +3,15 @@
 // 이름 공간 std::를 안해도 된다
 using namespace std;
 
-void call_by_pointer(int* val) {
+void call_by_reference(int& val) {
 	// 새로운 지역변수 val을 만들고 val가 가리키고 있는 대상의 값을 바꾼다
-	*val = 10;
+	val = 10;
 }
 
 int main() {
 	int num = 4;
 	// num의 값은 바뀐다.
-	call_by_pointer(&num);		
+	call_by_reference(num);
 	cout << num;
 	return 0;
 }
