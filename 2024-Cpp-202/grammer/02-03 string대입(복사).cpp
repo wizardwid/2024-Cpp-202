1169397ed813263d36435f1e0c7895594a2054d8
wizardwid <s2335@e-mirim.hs.kr>
2024-04-04 11:42:21 +0900

    string 대입2(C언어 방식)

--- 2024-Cpp-202/grammer/02-03 string대입(복사).cpp
+++ 2024-Cpp-202/grammer/02-03 string대입(복사).cpp
@@ -1,19 +1,11 @@
 ﻿#include <stdio.h>
+#include <string.h>
 
 int main(void) {
 	char str[30] = "delvelop hungry";
 	char str2[30] = "";
+
 	// str에 있는 내용을 str2에 복사
-	// '\0'을 만날 때까지
-	/*for (int i = 0; str[i] != '\0'; i++) {
-		str2[i] += str[i];
-	}
-	printf("%s", str2);*/
+	strcpy(str2, str);
 	
-	int i = 0;
-	for (; str[i] != '\0'; i++) {
-		str2[i] = str[i];
-	}
-	str2[i] = str[i];		//널문자('\0') 복사
-	printf("%s", str2);
 }
