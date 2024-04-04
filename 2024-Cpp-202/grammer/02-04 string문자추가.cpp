77fb33fda40a6a3c4f61d93cfd25d14d4c54a152
wizardwid <s2335@e-mirim.hs.kr>
2024-04-04 11:54:09 +0900

    string 문자열추가(C 방식)

---
+++ 2024-Cpp-202/grammer/02-04 string문자추가.cpp
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	char str[30] = "c";
+	//기존 문자열에다가 추가
+	strcat(str, "addition");
+
+	printf("%s ", str);
+	
+}
