eac8abccedddb4b1cdfa320a656bef00b95b349c
wizardwid <s2335@s-mirim.hs.kr>
2024-04-09 20:43:36 +0900

    생성자

---
+++ 2024-Cpp-202/grammer/03-01 구조체와 클래스.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+public:
+	// 생성자 : 객체가 생성될 때 호출되는 함수
+	// 생성자는 반환형이 없다.
+	Student(int hakbun, string name)
+	{
+		hakbun_ = hakbun;
+		name_ = name;
+	}
+private:
+	int hakbun_;
+	string name_;
+};
+
+int main(void)
+{
+	Student jisu = Student(2202, "나지수");
+	return 0;
+}
