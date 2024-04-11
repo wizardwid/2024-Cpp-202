00e421aa7a69aae1fd55ebe1f416e1a613523cef
wizardwid <s2335@e-mirim.hs.kr>
2024-04-11 10:45:52 +0900

    03-01 생성자 오버로딩

--- 2024-Cpp-202/grammer/03-01 구조체와 클래스.cpp
+++ 2024-Cpp-202/grammer/03-01 구조체와 클래스.cpp
@@ -6,6 +6,11 @@ class Student {
 public:
 	// 생성자 : 객체가 생성될 때 호출되는 함수
 	// 생성자는 반환형이 없다.
+	Student() {
+		hakbun_ = 2222;
+		name_ = "swag";
+	}
+
 	Student(int hakbun, string name)
 	{
 		hakbun_ = hakbun;
@@ -19,5 +24,6 @@ private:
 int main(void)
 {
 	Student jisu = Student(2202, "나지수");
+	Student swag = Student();
 	return 0;
 }
