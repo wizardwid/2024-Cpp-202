c2d1a5eb9e081cc2c6a80f19063f862159fea27e
wizardwid <s2335@e-mirim.hs.kr>
2024-05-07 15:53:27 +0900

    03-03 얕은복사2

--- 2024-Cpp-202/grammer/03-03 복사 생성자.cpp
+++ 2024-Cpp-202/grammer/03-03 복사 생성자.cpp
@@ -1,4 +1,5 @@
-﻿﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
 #include <string.h>
 
 using namespace std;
@@ -17,9 +18,15 @@ public:
 		cout << "생성자 호출 완료" << endl;
 	}
 
+	//복사생성자(얕은 복사)
+	Student(const Student& rhs) : hakbun_(rhs.hakbun_), name_(rhs.name_){
+		cout << "복사생성자 호출 완료" << endl;
+	}
+
 	// 소멸자 : 객체가 소멸될 때(메모리에서 지워질 때) 호출되는 함수
 	~Student(void)
 	{
+		//TODO: 얕은 복사로 인하여 이미 제거된 공간을 또 제거하려고 해서 에러 발생
 		delete[]name_;
 		cout << "소멸자 호출 완료" << endl;
 	}
@@ -39,7 +46,7 @@ private:
 int main(void)
 {
 	// 일반적인 생성자
-	Student stu = Student(2213, "조수빈");
+	Student stu = Student(2213, "JSB");
 	// 복사생성자
 	Student stu2 = stu;
 
