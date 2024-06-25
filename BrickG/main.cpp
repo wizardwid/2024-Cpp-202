be4a947a71ffeec757a2fcf097eaa47956138429
wizardwid <s2335@e-mirim.hs.kr>
2024-06-25 15:57:20 +0900

    다각형 지우기

--- BrickG/main.cpp
+++ BrickG/main.cpp
@@ -7,21 +7,10 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "bricks");
     window.setFramerateLimit(60);       //초당 프레임 60으로
 
-    // 원 객체 생성
-    sf::CircleShape circle(100.f);
-    circle.setFillColor(sf::Color::Magenta);
-    circle.setPosition(0.f, 0.f);
-
-    // 사각형 객체 생성
-    sf::RectangleShape rectangle(sf::Vector2f(200.f, 100.f));
-    rectangle.setFillColor(sf::Color::Blue);
-    rectangle.setPosition(0.f, 0.f);
-
     // 이벤트 루프 시작
     while (window.isOpen())
     {
         //update
-        rectangle.move(0.5f, 0);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -34,11 +23,6 @@ int main()
         //clear하지 않으면 잔상이 남아있는다
         // 화면 지우기
         window.clear(sf::Color::White);
-
-        //덧그리다
-        // 도형 그리기
-        window.draw(circle);
-        window.draw(rectangle);
 
         // 화면 업데이트
         window.display();
