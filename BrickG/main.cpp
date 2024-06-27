7c2c117ef7796616fd7ebdda277332591f19c5bf
wizardwid <s2335@e-mirim.hs.kr>
2024-06-27 11:54:39 +0900

    paddle 추가

--- BrickG/main.cpp
+++ BrickG/main.cpp
@@ -11,7 +11,7 @@ public:
         shape.setRadius(10.f);
         shape.setFillColor(sf::Color::Red);
         shape.setOrigin(10.f, 10.f);
-        velocity = { -8.f, -8.f };  //왼쪽 위쪽
+        velocity = { -8.f, -8.f };  // 왼쪽 위쪽
     }
 
     void update() {
@@ -30,29 +30,64 @@ public:
     float bottom() { return shape.getPosition().y + shape.getRadius(); }
 };
 
+// Paddle 클래스 정의
+class Paddle {
+public:
+    sf::RectangleShape shape;
+    const float paddleWidth = 100.f;
+    const float paddleHeight = 20.f;
+    const float paddleVelocity = 10.f;
+
+    Paddle(float mX, float mY) {
+        shape.setPosition(mX, mY);
+        shape.setSize({ paddleWidth, paddleHeight });
+        shape.setFillColor(sf::Color::Blue);
+        // 중심을 기준으로
+        shape.setOrigin(paddleWidth / 2.f, paddleHeight / 2.f);
+    }
+
+    void update() {
+        //왼쪽 화살키를 누르고 && 왼쪽 끝에 도달하지 않은 경우
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) && left() > 0) {
+            shape.move(-paddleVelocity, 0.f);
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) && right() < 800) {
+            shape.move(paddleVelocity, 0.f);
+        }
+    }
+
+    float left() { return shape.getPosition().x - shape.getSize().x / 2.f; }
+    float right() { return shape.getPosition().x + shape.getSize().x / 2.f; }
+    // 지금 당장은 쓸모가 없지만, 확장을 고려해서 일단 놔두겠다.
+    float top() { return shape.getPosition().y - shape.getSize().y / 2.f; }
+    float bottom() { return shape.getPosition().y + shape.getSize().y / 2.f; }
+};
+
 int main()
 {
-    //init
-    // 창 생성
-    sf::RenderWindow window(sf::VideoMode(800, 600), "bricks");
-    window.setFramerateLimit(60);       //초당 프레임 60으로
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Bricks");
+    window.setFramerateLimit(60);
 
-    // 이벤트 루프 시작
+    Ball ball(400.f, 300.f);
+    Paddle paddle(400.f, 550.f);
+
     while (window.isOpen())
     {
-        //update
         sf::Event event;
         while (window.pollEvent(event))
         {
-            // x 마크를 누르면 윈도우 창이 닫아진다.
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        //draw
-        //clear하지 않으면 잔상이 남아있는다
+        // update
+        paddle.update();
+
         // 화면 지우기
         window.clear(sf::Color::White);
+
+        // 그리기
+        window.draw(paddle.shape);
 
         // 화면 업데이트
         window.display();
