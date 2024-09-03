0939692061f51fdc0158211102c8260e75dbbb3d
wizardwid <s2335@e-mirim.hs.kr>
2024-09-03 15:48:23 +0900

    프론트창

--- 2024-Cpp-202/grammer/다형성예제.cpp
+++ 2024-Cpp-202/grammer/다형성예제.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -78,9 +79,47 @@ void main(void) {
 	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
 	Food* friends = new Cheese(100, 20, "짜계치", 100, 20, 100);
 
-	player->show();
-	cout << endl << endl;
-	friends->show();
+	while (true) {
+		system("cls");
+		cout << "나" << endl;
+		player->show();
+
+		cout << endl << endl;
+		cout << "상대방" << endl;
+		friends->show();
+
+		cout << "----------------------------------" << endl;
+
+		int select;
+		cout << "1. 공격" << endl;
+		cout << "2. 특수1" << endl;
+		cout << "3. 특수2" << endl;
+		cout << "4. 방어" << endl;
+
+		cin >> select;
+
+		switch (select) {
+			case 1:
+				//TODO : 공격
+				cout << "공격" << endl;
+				break;
+			case 2:
+				//TODO : 특수1
+				cout << "특수1" << endl;
+				break;
+			case 3:
+				//TODO : 특수2
+				cout << "특수2" << endl;
+				break;
+			case 4:
+				//TODO : 방어
+				cout << "방어" << endl;
+				break;
+			default:
+				cout << "default" << endl;
+		}
+		system("pause");
+	}
 
 	delete friends;
 	delete player;
