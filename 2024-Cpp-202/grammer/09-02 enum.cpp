2891d094d26131ca728b1e57d4a957834584496c
wizardwid <s2335@e-mirim.hs.kr>
2024-09-26 11:12:29 +0900

    09-02 enum 적용 후

--- 2024-Cpp-202/grammer/09-02 enum.cpp
+++ 2024-Cpp-202/grammer/09-02 enum.cpp
@@ -3,19 +3,26 @@
 
 using namespace std;
 
+//열거형 정의
+enum FoodStyle {
+	Korean,		// 0(0은 디폴트 값이고 값을 주고 싶으면 Korean=0으로 한다)
+	Japanese,	// 1
+	Chinese		// 2
+};	
+
 void main() {
-	// 0 : 한식, 1 : 일식, 2 : 중식
-	int style = 0;
+	// 열거형을 사용하면 가독성도 좋아지고, 중간에 값을 유연하게 추가할 수 있다
+	int style = Korean;
 
 	switch (style)
 	{
-	case 0:
+	case Korean:
 		cout << "김치, 쌈, 비빔밥" << endl;
 		break;
-	case 1:
+	case Japanese:
 		cout << "초밥, 우동, 타코야끼" << endl;
 		break;
-	case 2:
+	case Chinese:
 		cout << "동파육, 마라탕후루" << endl;
 		break;
 	default:
