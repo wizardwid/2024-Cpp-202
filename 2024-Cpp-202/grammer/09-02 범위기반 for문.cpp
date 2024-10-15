ad0636b6b0f236b94ac995b38a75b6732d64c803
wizardwid <s2335@e-mirim.hs.kr>
2024-10-15 14:24:22 +0900

    09-02 범위기간 for문 적용

--- 2024-Cpp-202/grammer/09-02 범위기반 for문.cpp
+++ 2024-Cpp-202/grammer/09-02 범위기반 for문.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void main() {
 	vector <int> v = { 445, 14, 27, 456, 23 };
 
-	for (int i = 0; i < v.size(); i++) {
-		cout << v[i] << endl;
+	for (int value : v) {
+		cout << value << endl;
 	}
 }
