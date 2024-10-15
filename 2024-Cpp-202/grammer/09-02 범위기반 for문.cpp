1eedd88e0cea7abddfc23a96d67a0ccac8d65926
wizardwid <s2335@e-mirim.hs.kr>
2024-10-15 14:20:22 +0900

    09-02 전통적인 반복문

---
+++ 2024-Cpp-202/grammer/09-02 범위기반 for문.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void main() {
+	vector <int> v = { 445, 14, 27, 456, 23 };
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << endl;
+	}
+}
