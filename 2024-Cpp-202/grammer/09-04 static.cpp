e96b556bdcd41a4a3553bde6abade5a89c918749
wizardwid <s2335@e-mirim.hs.kr>
2024-10-15 15:27:13 +0900

    09-04 static 기초

---
+++ 2024-Cpp-202/grammer/09-04 static.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class SW2_2 {
+public:
+	SW2_2(string name) :name_(name) {
+
+	}
+	static string get_teacher(void) { return teacher_; }
+	static int get_num(void) { return num_; }
+	string get_name(void) { return name_; }
+private:
+	static string teacher_;	//´ãÀÓ¼±»ı´Ô
+	static int num_;
+	string name_;	
+};
+
+string SW2_2::teacher_ = "±ÇÁö¿õ";
+int SW2_2::num_ = 0;
+
+void main() {
+	SW2_2* sb = new SW2_2("Jo Subin");
+	cout << "number : " << sb->get_num() << endl;
+	SW2_2* js = new SW2_2("Na Jisu");
+	cout << "number : " << js->get_num() << endl;
+
+	delete js;
+	delete sb;
+}
