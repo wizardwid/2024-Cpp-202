9e78a5284928fca802ab2c51a2b3afff76ce9e43
wizardwid <s2335@e-mirim.hs.kr>
2024-10-17 11:57:47 +0900

    [sG] 플레이어 띄우기

---
+++ shootingG/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
+
+void main(void) {
+	RenderWindow window(VideoMode(1000, 800), "shootingG");
+
+	while (window.isOpen()) {
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		RectangleShape player;
+		player.setSize(Vector2f(181.f, 75.f));
+		player.setFillColor(Color::Magenta);
+		player.setPosition(500.f, 400.f);
+
+		window.clear();
+		window.draw(player);
+		window.display();
+	}
+}
